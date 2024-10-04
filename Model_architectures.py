0c9643e56c5a124c97cc48f8a7b3201330e34a39
Anonimus_name <183583880+interpretable-cfs@users.noreply.github.com>
2024-10-04 14:54:11 +0200

    Update Model_architectures.py

--- Model_architectures.py
+++ Model_architectures.py
@@ -1,3 +1,33 @@
+import numpy as np
+import pandas as pd
+import json
+import argparse
+import datetime
+import math
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+from scipy.stats import mode
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import shutil
+
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.optimizers import Adam
+
+import np_utils
+from tensorflow.keras.models import load_model
+from tensorflow.keras.models import Model, model_from_json, Sequential
+from tensorflow.keras.layers import Lambda
+from tensorflow.keras import losses
+
 
 class ConvEncNet(tf.keras.Model):
     def __init__(self, latent_dim=20, num_channels=1, activation_type='relu', task='D'):
